67ffe9ac25be67576092a354dd3b0d39ecee5ba6
debojyotichatterjee <debojyoti.chatterjee9@gmail.com>
2021-06-02 17:38:09 +0530

    fixing menu item select

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Menu from './components/Menu/Menu';
 function App() {
   return (
     <div className={classes.App}>
-      <header className={classes["App-header"]}>
+      {/* <header className={classes["App-header"]}>
         <img src={logo} className={classes["App-logo"]} alt="logo" />
         <p>
           <code>Built with React</code>
         </p>
-      </header>
+      </header> */}
       <Menu />
     </div>
   );
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,46 +1,110 @@
-import React from "react";
-import classes from "./Menu.module.css"
+import React, { useState } from "react";
+import "./Menu.css"
 
 const Menu = () => {
+    const [activeFlag, setActiveFlag] = useState({
+        one: false,
+        two: false,
+        three: false,
+        four: false,
+        five: false
+    })
+    const menuClickHandler = (event) => {
+        console.log(event)
+        const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
+        const body = document.body;
+        const menu = body.querySelector(".menu");
+        const menuItems = menu.querySelectorAll(".menu__item");
+        const menuBorder = menu.querySelector(".menu__border");
+        let activeItem = menu.querySelector(".active");
+        menu.style.removeProperty("--timeOut");
+        let item = event.target.element;
+        let id = event.target.id;
+        console.log(id)
+        setActiveFlag(prevState => {
+            let val = {
+                one: false,
+                two: false,
+                three: false,
+                four: false,
+                five: false
+            }
+            val[id] = true
+            return val;
+        })
+        console.log(activeFlag)
+
+        // if (activeItem == item) return;
+
+        // if (activeItem) {
+        //     activeItem.classList.remove("active");
+        // }
+
+
+        // item.classList.add("active");
+        // body.style.backgroundColor = bgColorsBody[2];
+        // activeItem = item;
+        // offsetMenuBorder(activeItem, menuBorder);
+    }
     return (
         <React.Fragment>
-            <menu className={classes.menu}>
+            <menu id="menu_bar" className="menu">
 
-                <button className={`${classes.menu__item} ${classes.active}`} style={{ "--bgColorItem": "#ff8c00" }} >
-                    <svg className={classes.icon} viewBox="0 0 24 24">
+                <button
+                    id="one"
+                    className={activeFlag.one ? "menu__item active" : "menu__item"}
+                    style={{ "--bgColorItem": "#ff8c00" }}
+                    onClick={(e) => menuClickHandler(e)}>
+                    <svg className="icon" viewBox="0 0 24 24">
                         <path d="M3.8,6.6h16.4" />
                         <path d="M20.2,12.1H3.8" />
                         <path d="M3.8,17.5h16.4" />
                     </svg>
                 </button>
 
-                <button className={classes.menu__item} style={{ "--bgColorItem": "#f54888" }}>
-                    <svg className={classes.icon} viewBox="0 0 24 24">
+                <button
+                    id="two"
+                    className={activeFlag.two ? "menu__item active" : "menu__item"}
+                    style={{ "--bgColorItem": "#f54888" }}
+                    onClick={(e) => menuClickHandler(e)}>
+                    <svg className="icon" viewBox="0 0 24 24">
                         <path d="M6.7,4.8h10.7c0.3,0,0.6,0.2,0.7,0.5l2.8,7.3c0,0.1,0,0.2,0,0.3v5.6c0,0.4-0.4,0.8-0.8,0.8H3.8
         C3.4,19.3,3,19,3,18.5v-5.6c0-0.1,0-0.2,0.1-0.3L6,5.3C6.1,5,6.4,4.8,6.7,4.8z"/>
                         <path d="M3.4,12.9H8l1.6,2.8h4.9l1.5-2.8h4.6" />
                     </svg>
                 </button>
 
-                <button className={classes.menu__item} style={{ "--bgColorItem": "#4343f5" }}>
-                    <svg className={classes.icon} viewBox="0 0 24 24">
-                    <path d="M3.4,11.9l8.8,4.4l8.4-4.4" />
-                    <path d="M3.4,16.2l8.8,4.5l8.4-4.5" />
-                    <path d="M3.7,7.8l8.6-4.5l8,4.5l-8,4.3L3.7,7.8z" />
+                <button
+                    id="three"
+                    className={activeFlag.three ? "menu__item active" : "menu__item"}
+                    style={{ "--bgColorItem": "#4343f5" }}
+                    onClick={(e) => menuClickHandler(e)}>
+                    <svg className="icon" viewBox="0 0 24 24">
+                        <path d="M3.4,11.9l8.8,4.4l8.4-4.4" />
+                        <path d="M3.4,16.2l8.8,4.5l8.4-4.5" />
+                        <path d="M3.7,7.8l8.6-4.5l8,4.5l-8,4.3L3.7,7.8z" />
                     </svg>
                 </button>
 
-                <button className={classes.menu__item} style={{ "--bgColorItem": "#e0b115" }}>
-                    <svg className={classes.icon} viewBox="0 0 24 24">
-                    <path d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
+                <button
+                    id="four"
+                    className={activeFlag.four ? "menu__item active" : "menu__item"}
+                    style={{ "--bgColorItem": "#e0b115" }}
+                    onClick={(e) => menuClickHandler(e)}>
+                    <svg className="icon" viewBox="0 0 24 24">
+                        <path d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
           C3.9,4.4,4.4,3.9,5.1,3.9z"/>
-                    <path d="M4.2,9.3h15.6" />
-                    <path d="M9.1,9.5v10.3" />
+                        <path d="M4.2,9.3h15.6" />
+                        <path d="M9.1,9.5v10.3" />
                     </svg>
                 </button>
 
-                <button className={classes.menu__item} style={{ "--bgColorItem": "#65ddb7" }}>
-                    <svg className={classes.icon} viewBox="0 0 24 24" >
+                <button
+                    id="five"
+                    className={activeFlag.five ? "menu__item active" : "menu__item"}
+                    style={{ "--bgColorItem": "#65ddb7" }}
+                    onClick={(e) => menuClickHandler(e)}>
+                    <svg className="icon" viewBox="0 0 24 24" >
                         <path d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
           C3.9,4.4,4.4,3.9,5.1,3.9z"/>
                         <path d="M5.5,20l9.9-9.9l4.7,4.7" />
@@ -48,11 +112,11 @@ const Menu = () => {
                     </svg>
                 </button>
 
-                <div className={classes.menu__border}></div>
+                <div className="menu__border"></div>
 
             </menu>
 
-            <div className={classes["svg-container"]}>
+            <div className="svg-container">
                 <svg viewBox="0 0 202.9 45.5" >
                     <clipPath id="menu" clipPathUnits="objectBoundingBox" transform="scale(0.0049285362247413 0.021978021978022)">
                         <path d="M6.7,45.5c5.7,0.1,14.1-0.4,23.3-4c5.7-2.3,9.9-5,18.1-10.5c10.7-7.1,11.8-9.2,20.6-14.3c5-2.9,9.2-5.2,15.2-7
